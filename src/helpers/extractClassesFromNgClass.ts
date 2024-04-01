c2f241c105773ca2ff4039525ddfc71903df5122
LORDBABUINO <jorge.x7@gmail.com>
2024-04-01 00:47:16 -0300

    Refactor function "extractClassesFromNgClass"

--- src/helpers/extractClassesFromNgClass.ts
+++ src/helpers/extractClassesFromNgClass.ts
@@ -3,18 +3,10 @@
  * @param {string} value
  */
 export default function extractClassesFromNgClass(value: string) {
-  const found = [];
-  const rxp = /{([^}]+)}/g;
-  let curMatch;
-
-  while ((curMatch = rxp.exec(value))) {
-    found.push(curMatch[1].replace(/\n/g, '').replace(/ /g, ''));
-  }
-
-  let classes: string[] = [];
-  if (found.length > 0) {
-    classes = found[0].split(',').map((e) => e.split(':')[0]);
-  }
-
-  return classes;
+  return [...value.matchAll(/{([^}]+)}/g)].flatMap((match) =>
+    match[1]
+      .replace(/[\n ]/g, '')
+      .split(',')
+      .map((e) => e.split(':')[0])
+  );
 }
