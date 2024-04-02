01c085da8cdc18e0101688f877644a23b536cd31
LORDBABUINO <jorge.x7@gmail.com>
2024-04-02 01:46:32 -0300

    put docs on whitelist function

--- src/helpers/whitelist.ts
+++ src/helpers/whitelist.ts
@@ -3,6 +3,15 @@ import { Ignore } from '../config';
 import { SELECTORS_TO_IGNORE } from '../constants';
 import { handler } from './whitelist/handler';
 
+/**
+ * Filters a list of CSS classes by removing those that are specified in the ignore list or match patterns from an Ignore object associated with the current CSS file path.
+ *
+ * @param classes - The list of CSS class names to be filtered.
+ * @param cssPath - The path to the current CSS file being processed.
+ * @param ignore - An array of strings or Ignore objects specifying classes or patterns to ignore.
+ * @param projectPath - The root path of the project, used to resolve relative file paths.
+ * @returns A filtered array of CSS class names that are not ignored based on the provided rules.
+ */
 export default function whitelist(
   classes: string[],
   cssPath: string,
